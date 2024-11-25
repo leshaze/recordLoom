96f01e8da61acf5a93074bb3176ff95ce25bedf8
Leshaze <n.r.hilse@googlemail.com>
2024-11-25 22:35:46 +0100

    price_history

--- database/migrations/2024_10_31_203228_create_price_histories_table.php
+++ database/migrations/2024_10_31_203228_create_price_histories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('price_histories', function (Blueprint $table) {
+        Schema::create('price_history', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('price');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('price_histories');
+        Schema::dropIfExists('price_history');
     }
 };
